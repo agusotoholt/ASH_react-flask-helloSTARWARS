00320452e5131ae9b7e4f4fa99a313d7ee02398b
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-29 15:25:08 +0000

    rutas back creadas

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +13,53 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    get_user = User.query.filter_by(email=email).first()
 
+    if not get_user:
+        return jsonify({"msg": "User does not exist"}), 401
+    if email != get_user.email or password != get_user.password:
+        return jsonify({"msg": "Bad email or password"}), 401
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    # Get and Show user profile data
+    results_query = User.query.filter_by(email=current_user).first()
+    if not results_query:
+        return jsonify({"error": "User not found"}), 404
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "msg": "Your user profile:",
+        "results": results_query.serialize()
     }
-
     return jsonify(response_body), 200
+
+# Create a route to signup a new user.
+@api.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "no data"}), 404
+
+    check_email = User.query.filter_by(email = data['email']).first()
+    if check_email:
+        return jsonify({"error": "Email address already exists"}), 404
+    
+    new_user = User(email = data["email"], is_active = True, password = data["password"])
+
+    db.session.add(new_user)
+    db.session.commit()
+    # new_user_add = new_user.serialize()
+
+    return jsonify({"msg": "New user created", "new user": new_user.serialize()}), 200
