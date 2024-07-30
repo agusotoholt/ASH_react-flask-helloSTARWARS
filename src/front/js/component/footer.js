031f56690b59d576a93e34e74415d908e7a68e8e
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-30 21:07:23 +0000

    authentificacion ok

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
+		<p><Link to="/signup"><button type="button" className="btn btn-link">Sign Up</button></Link></p>
 		<p>
 			Made with <i className="fa fa-heart text-success" /> by{" "}
 			<a href="www.linkedin.com/in/agustin-soto-holt">agusotoholt</a>
